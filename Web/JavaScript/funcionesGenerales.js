21f811046c09fd575e2ac028cde6653e615a5dcc
Rasta23Mas <rasta23mas@gmail.com>
2020-11-03 12:40:53 -0600

    cierre de caja indispensable

--- Web/JavaScript/funcionesGenerales.js
+++ Web/JavaScript/funcionesGenerales.js
@@ -218,6 +218,26 @@ function fnMesNombre(mes) {
    return nombreDelMes;
 
 }
+
+function fnCierreCajaIndispensable(estatus,user,tipo) {
+    var dataEnviar = {
+        "estatus": estatus,
+        "user": user,
+        "tipo": tipo,
+    };
+    $.ajax({
+        data: dataEnviar,
+        url: '../../../com.Mexicash/Controlador/Cierre/ConCierreCajaIndispensable.php',
+        type: 'post',
+        dataType: "json",
+        success: function (response) {
+            if (response > 0) {
+                alertify.success("Se actualizo cierre de caja.")
+            }
+        },
+    })
+}
+
 //Saber el tipo de dato
 //alert(typeof (totalVencInteres))
 //Obtener el valor texto de un select
